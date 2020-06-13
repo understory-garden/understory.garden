8fedc9206e1eb2e5601fae4c4c3fca34af13c1ce
Travis Vachon <travis.vachon@gmail.com>
2020-06-12 21:41:09 -0700

    proof of concept using swr to load Solid resources

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,37 @@
 import Head from 'next/head'
 
+import useSWR from 'swr'
+import auth from 'solid-auth-client'
+
+import useWebId from "../hooks/useWebId"
+
+
+function AuthButton(){
+  const webId = useWebId()
+  if (webId === undefined){
+    return <div>loading...</div>
+  } else if (webId === null) {
+    return (
+      <button onClick={() => auth.popupLogin({ popupUri: "/popup.html" })}>
+        Log In
+      </button>
+    )
+  } else {
+    return <button onClick={() => auth.logout()}>Log Out</button>
+  }
+}
+
+function Profile(){
+  const webId = useWebId()
+  const { data } = useSWR(useWebId, auth.fetch)
+  if (data){
+    return <div>hello, {data.name}</div>
+  } else {
+    return <div>loading...</div>
+  }
+
+}
+
 export default function Home() {
   return (
     <div className="container">
@@ -9,43 +41,11 @@ export default function Home() {
       </Head>
 
       <main>
+        <AuthButton/>
+        <Profile />
         <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer>
