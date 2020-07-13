c85e3fdf9850f613f436084b957686a3d2926f58
Travis Vachon <travis.vachon@gmail.com>
2020-07-13 13:05:13 -0700

    basic space/flow/module layout
    plus a cool gradient

--- pages/index.js
+++ pages/index.js
@@ -28,12 +28,22 @@ function AuthButton(){
   }
 }
 
+function Space({className, ...props}){
+  return <div className={`flex flex-column bg-gradient-tl-background w-screen h-screen p-6 ${className}`} {...props}/>
+}
+
+function Flow({className, ...props}){
+  return <div className={`flex flex-row h-84 ${className}`} {...props}/>
+}
+
+function Module({className, ...props}){
+  return <div className={`m-6 rounded-lg bg-white bg-opacity-25 border-white border-opacity-25 border-solid border-8 ${className}`} {...props}/>
+}
 
 function Profile(){
   const webId = useWebId()
   const {thing: profile, save: saveProfile} = useThing(webId)
   const name = profile && getStringNoLocaleOne(profile, foaf.name)
-  const knows = profile && getUrlAll(profile, foaf.knows)
   const profileImage = profile && getUrlOne(profile, vcard.hasPhoto)
   const [saving, setSaving] = useState(false)
   if (profile){
@@ -43,19 +53,40 @@ function Profile(){
           hello, {name}
         </div>
         {profileImage && <img src={profileImage}/>}
+      </>
+    )
+  } else {
+    return <div>loading...</div>
+  }
+
+}
+
+function Friends(){
+  const webId = useWebId()
+  const {thing: profile, save: saveProfile} = useThing(webId)
+  const name = profile && getStringNoLocaleOne(profile, foaf.name)
+  const knows = profile && getUrlAll(profile, foaf.knows)
+  const profileImage = profile && getUrlOne(profile, vcard.hasPhoto)
+  const [saving, setSaving] = useState(false)
+  if (profile){
+    return (
+      <>
         {knows && knows.map(url => (
-          <p key={url}>{url}</p>
-        ))}
-        <Button onClick={
+          <div key={url}>
+            {url}
+            <Button onClick={
                   async () => {
                     setSaving(true)
-                    await saveProfile(removeUrl(profile, foaf.knows, "https://lordvacon.inrupt.net/profile/card#me"))
+                    await saveProfile(removeUrl(profile, foaf.knows, url))
                     setSaving(false)
                   }}
-                disabled={saving}
-        >
-          remove knows
-        </Button>
+                    disabled={saving}
+            >
+              remove knows
+            </Button>
+          </div>
+        ))}
+
         <Button onClick={
                   async () => {
                     setSaving(true)
@@ -83,166 +114,23 @@ export default function Home() {
       </Head>
 
       <main>
-        <AuthButton/>
-        <Profile />
+        <Space>
+          <Flow>
+            <Module>
+              <Profile />
+            </Module>
+            <Module>
+              <Friends />
+            </Module>
+            <Module>
+              <AuthButton/>
+            </Module>
+          </Flow>
+        </Space>
       </main>
-
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+
       </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      }
+    },
+    gradientColors: {
+      background: ['#d57eeb', '#fccb90']
+    },
+    linearGradientColors: theme => theme('gradientColors'),
+    radialGradientColors: theme => theme('gradientColors'),
+    conicGradientColors: theme => theme('gradientColors'),
   },
   variants: {
 
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-gradients'),
+  ],
 }
