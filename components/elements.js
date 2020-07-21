3aa742c4b9f4e898cd32f9484a4dcfc8dfb397ae
Travis Vachon <travis.vachon@gmail.com>
2020-07-21 15:08:54 -0700

    remove dialog components
    these were an experiment and raen't used

--- components/elements.js
+++ components/elements.js
@@ -1,7 +1,6 @@
 import Button from "./Button"
-import Dialog, { DialogContent, DialogActions } from "./Dialog"
 import ReactLoader from 'react-loader-spinner'
 
 export const Loader = () => <ReactLoader type="Puff" color="white" />
 
-export { Button, Dialog, DialogContent, DialogActions }
+export { Button }
