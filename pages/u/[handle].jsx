c74a411cf33f6e946d0b81da1b92f56ae1c40ea7
Travis Vachon <travis.vachon@gmail.com>
2020-07-30 16:13:57 -0700

    display images and posts on profile page

--- pages/u/[handle].jsx
+++ pages/u/[handle].jsx
@@ -4,13 +4,17 @@ import {
   getUrlAll, removeUrl, addUrl,
   getStringNoLocaleOne, getUrlOne
 } from '@solid/lit-pod'
+import ReactMarkdown from "react-markdown"
 import { useRouter } from 'next/router'
+import { schema } from "rdf-namespaces"
 
-import useThing from "~hooks/useThing"
 import { Space, Flow, Module } from "~components/layout"
 import { Button, Loader } from "~components/elements"
 import useWebId from "~hooks/useWebId"
+import { useImagesContainerUri, usePostsContainerUri } from "~hooks/uris"
+import useThing, { useContainer } from "~hooks/useThing"
 
+import { byDctModified } from "~lib/sort"
 
 function AddKnows({ webId }) {
   const myWebId = useWebId()
@@ -42,7 +46,54 @@ function handleToWebId(handle) {
   }
 }
 
-export default function OtherProfile() {
+function ImageModule({ resource }) {
+  return (
+    <Module>
+      <img src={resource.url} className="object-contain h-full" alt="no description" />
+    </Module>
+  )
+}
+
+function PostModule({ resource }) {
+  const { thing: post } = useThing(`${resource.url}#post`)
+  const title = post && getStringNoLocaleOne(post, schema.headline)
+  const body = post && getStringNoLocaleOne(post, schema.articleBody)
+
+  return (
+    <Module>
+      <div className="absolute inset-0 mt-6 p-3 prose overflow-y-scroll">
+        <h2>{title}</h2>
+        <ReactMarkdown source={body} />
+      </div>
+    </Module>
+  )
+}
+
+function ImagesFlow() {
+  const imagesContainerUri = useImagesContainerUri("public")
+  const { resources } = useContainer(imagesContainerUri)
+  return (
+    <Flow>
+      {resources && resources.sort(byDctModified).reverse().map(resource => (
+        <ImageModule key={resource.url} resource={resource} />
+      ))}
+    </Flow>
+  )
+}
+
+function PostsFlow() {
+  const postContainerUri = usePostsContainerUri("public")
+  const { resources } = useContainer(postContainerUri)
+  return (
+    <Flow>
+      {resources && resources.sort(byDctModified).reverse().map(resource => (
+        <PostModule key={resource.url} resource={resource} />
+      ))}
+    </Flow>
+  )
+}
+
+export default function UserProfile() {
   const router = useRouter()
   const { handle } = router.query
   const webId = handleToWebId(handle)
@@ -52,7 +103,7 @@ export default function OtherProfile() {
   if (profile) {
     return (
       <Space>
-        <Flow className="h-full w-full">
+        <Flow>
           <Module className="p-6 flex-grow">
             <div className="flex flex-row">
               {profileImage && <img className="w-64 m-4" src={profileImage} alt={name} />}
@@ -61,9 +112,10 @@ export default function OtherProfile() {
                 <AddKnows webId={webId} />
               </div>
             </div>
-
           </Module>
         </Flow>
+        <ImagesFlow />
+        <PostsFlow />
       </Space>
     )
   } else {
