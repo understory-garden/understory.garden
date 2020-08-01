3f2ac87928854414f1c6570240f4a65eae05847c
Travis Vachon <travis.vachon@gmail.com>
2020-07-31 22:59:25 -0700

    initial work toward sharing dialog

--- src/hooks/useThing.js
+++ src/hooks/useThing.js
@@ -1,27 +1,68 @@
 import useSWR from 'swr'
 import {
-  fetchLitDataset, getThingOne, saveLitDatasetAt, setThing, getUrlAll
+  fetchLitDataset, getThingOne, saveLitDatasetAt, setThing, getUrlAll,
+  unstable_fetchLitDatasetWithAcl,
+  unstable_fetchFile, unstable_overwriteFile, unstable_fetchFileWithAcl
 } from '@itme/solid-client'
 import { ldp } from "rdf-namespaces"
 
 import equal from 'fast-deep-equal/es6'
 
-function useThing(uri, { compare, ...options } = {}) {
+export function useFile(uri, { compare, acl, ...options } = {}) {
+  const fetch = acl ? unstable_fetchFileWithAcl : unstable_fetchFile
+
+  options.compare = compare || equal
+
+  const { data: file, mutate, ...rest } = useSWR(uri, fetch, options)
+  const save = async (blob) => {
+    mutate(blob, false)
+    const savedDataset = await unstable_overwriteFile(uri, blob)
+    mutate(blob)
+    return savedDataset
+  }
+  return (
+    {
+      file,
+      mutate,
+      save,
+      ...rest
+    }
+  )
+}
+
+export function useResource(uri, { compare, acl, ...options } = {}) {
+  const fetch = acl ? unstable_fetchLitDatasetWithAcl : fetchLitDataset
+
+  options.compare = compare || equal
+
+  const { data: resource, mutate, ...rest } = useSWR(uri, fetch, options)
+  const save = async (newDataset) => {
+    mutate(newDataset, false)
+    const savedDataset = await saveLitDatasetAt(uri, newDataset)
+    mutate(savedDataset)
+    return savedDataset
+  }
+  return (
+    {
+      resource,
+      mutate,
+      save,
+      ...rest
+    }
+  )
+}
+
+function useThing(uri, options = {}) {
   const documentURL = uri && new URL(uri)
   if (documentURL) {
     documentURL.hash = ""
   }
   const documentUri = documentURL && documentURL.toString()
-  options.compare = compare || equal
-
-  const { data, mutate, ...rest } = useSWR(documentUri, fetchLitDataset, options)
-  const thing = data && getThingOne(data, uri)
+  const { resource, mutate, save: saveResource, ...rest } = useResource(uri, options)
+  const thing = resource && getThingOne(resource, uri)
   const save = async (newThing) => {
-    const newDataset = setThing(data, newThing)
-    mutate(newDataset, false)
-    const savedDataset = await saveLitDatasetAt(documentUri, newDataset)
-    mutate(savedDataset)
-    return savedDataset
+    const newDataset = setThing(resource, newThing)
+    return saveResource(documentUri, newDataset)
   }
   return (
     {
