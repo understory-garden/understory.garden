7afa675cf4fa9cd82cb6daa79e271096d374afda
Travis Vachon <travis.vachon@gmail.com>
2020-12-24 13:00:14 -0800

    Add basic formatting utils and name changer

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
+import { useAuthentication, useLoggedIn, useMyProfile, useProfile, useWebId, useEnsured } from 'swrlit'
+import {
+  setStringNoLocale, getStringNoLocale, getUrl, setUrl
+} from '@inrupt/solid-client'
+import { FOAF, AS, RDF, RDFS } from '@inrupt/vocab-common-rdf'
+import { WS } from '@inrupt/vocab-solid-common'
+
 import Nav from '../components/nav'
-import { useAuthentication, useLoggedIn } from 'swrlit'
 
 function LoginUI(){
   const [handle, setHandle] = useState("")
@@ -39,17 +45,46 @@ function LoginUI(){
   )
 }
 
+export function useStorageContainer(webId) {
+  const { profile } = useProfile(webId)
+  return profile && getUrl(profile, WS.storage)
+}
+
+export function useFacebabyContainerUri(webId, path = 'public') {
+  const storageContainer = useStorageContainer(webId)
+  return useEnsured(storageContainer && `${storageContainer}${path}/itme/facebaby/`)
+}
 
 export default function IndexPage() {
   const loggedIn = useLoggedIn()
+  const { profile, save: saveProfile } = useMyProfile()
+  const name = profile && getStringNoLocale(profile, FOAF.name)
+  const [newName, setNewName] = useState("")
+  async function onSave(){
+    return await saveProfile(setStringNoLocale(profile, FOAF.name, newName))
+  }
 
+  const webId = useWebId()
+  const appContainerUri = useFacebabyContainerUri(webId)
   return (
     <div className="bg-black h-screen">
       <Nav />
       { (loggedIn === true) ? (
-        <h1 className="text-6xl text-center bold font-logo text-white">
-          ANY QUESTIONS?
-        </h1>
+        <div>
+          {name && (
+            <h5 className="text-4xl text-center text-white font-logo">you are {name}</h5>
+          )}
+          <div className="flex flex-row m-auto justify-center">
+            <input value={newName} onChange={e => setNewName(e.target.value)} className="input-text mr-3" type="text" placeholder="New Name" />
+            <button className="btn text-white" onClick={onSave}>
+              Set Name
+            </button>
+          </div>
+
+          <h1 className="text-6xl text-center bold font-logo text-white">
+            ANY QUESTIONS?
+          </h1>
+        </div>
       ) : (
         (loggedIn === false) ? (
           <>
@@ -64,7 +99,9 @@ export default function IndexPage() {
             <LoginUI/>
           </>
         ) : (
-          <></>
+          <div>
+            loading....
+          </div>
         )
       ) }
     </div>
