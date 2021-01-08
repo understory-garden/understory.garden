acf09d88e40546500dc31ed87748af711116f8d9
Travis Vachon <travis.vachon@gmail.com>
2021-01-08 01:48:56 -0800

    add name to profile page

--- pages/u/[handle].jsx
+++ pages/u/[handle].jsx
@@ -1,16 +1,25 @@
 import { useRouter } from 'next/router'
+import { FOAF } from '@inrupt/vocab-common-rdf'
+import { getStringNoLocale } from '@inrupt/solid-client'
+import { useProfile } from 'swrlit'
+
 import { handleToWebId, profilePath } from "../../utils/uris"
 import Notes from '../../components/Notes'
 import Nav from '../../components/nav'
 
 export default function ProfilePage(){
   const router = useRouter()
-  const { query: { name, handle } } = router
+  const { query: { handle } } = router
   const webId = handleToWebId(handle)
+  const { profile } = useProfile(webId)
+  const name = profile && getStringNoLocale(profile, FOAF.name)
 
   return (
     <div className="bg-black text-white h-screen">
       <Nav />
+      {name && (
+        <h3 className="text-4xl text-center font-logo">{name}</h3>
+      )}
       <Notes path={profilePath(webId)}/>
     </div>
   )
