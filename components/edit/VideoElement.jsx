7624f4fd927a3cddb14826eddf4988f6cc7fe929
Travis Vachon <travis.vachon@gmail.com>
2021-01-15 21:25:55 -0800

    show youtube videos when we find them

--- components/edit/VideoElement.jsx
+++ components/edit/VideoElement.jsx
@@ -10,13 +10,20 @@ export default function VideoElement({attributes, children, element}){
   function save(){
     Transforms.setNodes(editor, { url }, { at: ReactEditor.findPath(editor, element)})
   }
+  const youtube = element && element.url && element.url.includes("youtube.com")
+  const youtubeId = youtube && new URL(element.url).searchParams.get("v")
   return (
     <div {...attributes}>
       <div contentEditable={false} >
         {(readOnly || element.url) ? (
-          <video controls className="m-auto">
-            <source src={element.url} />
-          </video>
+          youtube ? (
+            <iframe width="560" height="315" src={`https://www.youtube.com/embed/${youtubeId}`}
+                    frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullscreen></iframe>
+          ) : (
+            <video controls className="m-auto">
+              <source src={element.url} />
+            </video>
+          )
         ) : (
           <div className="flex flex-row">
             <input type="url" placeholder="paste video url here..."
