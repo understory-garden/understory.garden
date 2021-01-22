015d5387c5e3c185b231fbbe674291e89aefe20c
Travis Vachon <travis.vachon@gmail.com>
2021-01-21 19:57:23 -0800

    tweak icon bar styling
    can't figure out how to make them line up, TODO: fix

--- components/IconButton.jsx
+++ components/IconButton.jsx
@@ -1,7 +1,7 @@
 const IconButton = ({active=true, className, ...props}) => {
   const activeClasses = active ? "bg-pink-200 hover:bg-pink-100" : "bg-gray-200 hover:bg-gray-100"
   return (
-    <button className={`${activeClasses} text-gray-900 px-1 ${className || ''}`} {...props}/>
+    <button className={`${activeClasses} h-full text-gray-900 px-1 ${className || ''}`} {...props}/>
   )
 }
 
