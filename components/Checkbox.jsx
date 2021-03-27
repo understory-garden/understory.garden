5848161467173123d45a08b01094e399fa949291
Travis Vachon <travis.vachon@gmail.com>
2021-03-26 17:58:42 -0700

    remove unused Checkbox.jsx

--- components/Checkbox.jsx
+++
@@ -1,8 +0,0 @@
-const Checkbox = () => (
-  <label class="inline-flex items-center mt-3">
-    <input type="checkbox" class="form-checkbox h-5 w-5 text-orange-600" checked/>
-    <span class="ml-2 text-gray-700">label</span>
-  </label>
-)
-
-export default Checkbox
