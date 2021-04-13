c77950e685043ea05711554ad23b2c29f9dcb132
Travis Vachon <travis.vachon@gmail.com>
2021-04-12 17:04:53 -0700

    configure SWR to not retry on errors
    this was causing tons of spurious retries after 404s, and is probably safer for now while the
    server may be flakey

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,22 +5,25 @@ import { AuthenticationProvider } from 'swrlit'
 import Head from 'next/head'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import { SWRConfig } from 'swr'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="stylesheet" href="https://use.typekit.net/ydt0qut.css"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="stylesheet" href="https://use.typekit.net/ydt0qut.css" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <meta name="monetization" content="$ilp.uphold.com/DYPhbXPmDa2P" />
       </Head>
-      <DndProvider backend={HTML5Backend}>
-        <AuthenticationProvider>
-          <Component {...pageProps} />
-        </AuthenticationProvider>
-      </DndProvider>
+      <SWRConfig value={{ shouldRetryOnError: false }}>
+        <DndProvider backend={HTML5Backend}>
+          <AuthenticationProvider>
+            <Component {...pageProps} />
+          </AuthenticationProvider>
+        </DndProvider>
+      </SWRConfig>
     </>
   )
 }
