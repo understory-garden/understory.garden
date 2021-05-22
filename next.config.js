4cb0d746ae2ec13ebb67f675afff341e0a6674ae
Travis Vachon <travis.vachon@gmail.com>
2021-05-21 21:49:10 -0700

    upgrade nextjs, tailwindcss
    also move them to dev deps where they belong

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+/*  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
 //    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
     config.optimization.minimize = false
     // Important: return the modified config
     return config
-  },
+  },*/
 }
