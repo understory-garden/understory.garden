6816aa0d0641b08dfc9b6ac0a632ccefe831fd3d
Travis Vachon <travis.vachon@gmail.com>
2021-05-28 14:16:02 -0700

    re-disable webpack minimization
    it breaks image-blob-reduce

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-/*  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
-//    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+    // config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+
+    // IMPORTANT: we need to disable minimization for now because image-blob-reduce
+    // has problems with terser minimization:
+    // https://github.com/nodeca/image-blob-reduce
     config.optimization.minimize = false
     // Important: return the modified config
     return config
-  },*/
+  },
 }
