c3ae3f37f161da53d42cfe694a4cb82e084f7614
Ian Davis <jungziege@gmail.com>
2021-08-05 22:45:43 -0700

    Add brand colors and brand fonts

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -21,6 +21,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
         <meta name="monetization" content="$ilp.uphold.com/DYPhbXPmDa2P" />
       </Head>
       <SWRConfig value={{ shouldRetryOnError: false }}>
