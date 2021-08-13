16981089a105ad810173efb5374dcefd1ea46d37
Travis Vachon <travis.vachon@gmail.com>
2021-08-13 16:36:19 -0700

    proof of concept hex grid
    needs lots of work and polish but works!

---
+++ pages/hex.jsx
@@ -0,0 +1,20 @@
+
+import Nav from '../components/nav'
+
+export default function HexGridPage() {
+  return (
+    <div className="page">
+      <Nav />
+      <div className="grid hex gap-2">
+        <div><div><h1>1</h1></div></div>
+        <div><div><h2>2</h2></div></div>
+        <div><div><h3>3</h3></div></div>
+        <div><div><h3>4</h3></div></div>
+        <div><div><h3>5</h3></div></div>
+        <div><div><h3>6</h3></div></div>
+        <div><div><h3>7</h3></div></div>
+        <div><div><h3>8</h3></div></div>
+      </div>
+    </div>
+  )
+}
