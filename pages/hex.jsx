6dcce4070f98187012706830c1aa126ff9bd096a
Ian Davis <jungziege@gmail.com>
2021-08-30 17:05:30 -0700

    Paired with Travis to center the hex titles

--- pages/hex.jsx
+++ pages/hex.jsx
@@ -1,19 +1,23 @@
 
 import Nav from '../components/nav'
 
+
+export function Hex({children}) {
+  return (
+    <div><div>
+      <div className="flex flex-col justify-center items-center">
+        {children}
+      </div>
+    </div></div>
+  )
+}
+
 export default function HexGridPage() {
   return (
     <div className="page">
       <Nav />
       <div className="grid hex gap-6 p-6">
-        <div><div><h1>1</h1></div></div>
-        <div><div><h2>2</h2></div></div>
-        <div><div><h3>3</h3></div></div>
-        <div><div><h3>4</h3></div></div>
-        <div><div><h3>5</h3></div></div>
-        <div><div><h3>6</h3></div></div>
-        <div><div><h3>7</h3></div></div>
-        <div><div><h3>8</h3></div></div>
+        {[...Array(9).keys()].map(n => <Hex><h1>NOTE {n}</h1></Hex>)}
       </div>
     </div>
   )
