9be545519dea54a82174f2c2c24b69fd8e6565cb
Ian Davis <jungziege@gmail.com>
2021-09-24 16:03:13 -0700

    Prettier reformat #cosmetic

--- components/Notes.jsx
+++ components/Notes.jsx
@@ -1,23 +1,29 @@
-import { useContext } from 'react'
+import { useContext } from "react";
 import {
-  setStringNoLocale, getStringNoLocale, getUrl, setUrl, createSolid, getThingAll, asUrl,
-  getDatetime
-} from '@inrupt/solid-client'
-import Link from 'next/link'
-import { DCTERMS } from '@inrupt/vocab-common-rdf'
-import WorkspaceContext from '../contexts/WorkspaceContext'
+  setStringNoLocale,
+  getStringNoLocale,
+  getUrl,
+  setUrl,
+  createSolid,
+  getThingAll,
+  asUrl,
+  getDatetime,
+} from "@inrupt/solid-client";
+import Link from "next/link";
+import { DCTERMS } from "@inrupt/vocab-common-rdf";
+import WorkspaceContext from "../contexts/WorkspaceContext";
 
-import { conceptIdFromUri } from '../model/concept'
-import { useConcepts } from '../hooks/concepts'
-import NoteContext from '../contexts/NoteContext'
-import { urlSafeIdToConceptName } from '../utils/uris'
 import NewNoteForm from './NewNoteForm'
+import { conceptIdFromUri } from "../model/concept";
+import { useConcepts } from "../hooks/concepts";
+import NoteContext from "../contexts/NoteContext";
+import { urlSafeIdToConceptName } from "../utils/uris";
 
 export function Note({ concept }) {
-  const uri = asUrl(concept)
-  const id = conceptIdFromUri(uri)
-  const name = urlSafeIdToConceptName(id)
-  const { path } = useContext(NoteContext)
+  const uri = asUrl(concept);
+  const id = conceptIdFromUri(uri);
+  const name = urlSafeIdToConceptName(id);
+  const { path } = useContext(NoteContext);
 
   return (
     <li className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200 overflow-x-scroll">
@@ -31,18 +37,21 @@ export function Note({ concept }) {
         </a>
       </Link>
     </li>
-  )
+  );
 }
 
 export function NotesFromConcepts({ path = "/notes", webId, concepts }) {
-  const { slug: workspaceSlug } = useContext(WorkspaceContext)
+  const { slug: workspaceSlug } = useContext(WorkspaceContext);
   return (
     <NoteContext.Provider value={{ path: `${path}/${workspaceSlug}` }}>
       <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {concepts && concepts.map(concept => <Note key={asUrl(concept)} concept={concept} />)}
+        {concepts &&
+          concepts.map((concept) => (
+            <Note key={asUrl(concept)} concept={concept} />
+          ))}
       </ul>
     </NoteContext.Provider>
-  )
+  );
 }
 
 export default function Notes({ path = "/notes", webId }) {
