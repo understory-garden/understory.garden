d559b1280ddc23d48f2fcfc7952f6055eb32797f
Travis Vachon <travis.vachon@gmail.com>
2021-09-24 12:05:39 -0700

    a few relatively minor tweaks to the new note dialog

--- components/modals/NewNote.jsx
+++ components/modals/NewNote.jsx
@@ -5,15 +5,15 @@ import { Close as CloseIcon } from '../icons'
 export default function NewNoteModal() {
   const [pub, setPublic] = useState(false)
   return (
-    <div className="rounded-lg overflow-hidden bg-white flex flex-column items-stretch">
-      <div className="flex flex-row justify-between h-18 p-6 bg-gray-500">
+    <div className="rounded-lg overflow-hidden bg-white flex flex-col items-stretch">
+      <div className="flex flex-row justify-between self-stretch h-18 p-6 bg-gray-500">
         <div className="flex flex-row justify-start items-start gap-4">
           <h2 className="text-white font-bold text-xl">New {pub ? 'Public' : 'Private'} Note</h2>
           <PrivacyToggle enabled={pub} setEnabled={setPublic} />
         </div>
-        <CloseIcon className="text-white"/>
+        <CloseIcon className="text-white h-6 w-6 flex-grow-0"/>
       </div>
-      <div className="flex flex-column ">
+      <div className="flex flex-col ">
         Note Name
       </div>
     </div>
